7210e0a44866526934a3af227b133183b243ccf1
Luke Richards <luke.richards@emailcenteruk.com>
2015-10-23 17:36:57 +0100

    Daemon test setup
    Some initial work on the daemon test case. This gets it initially
    working with further tests to be added to actually exercise the
    functionality.

--- src/Command/DaemonCommand.php
+++ src/Command/DaemonCommand.php
@@ -5,8 +5,6 @@ namespace Phlib\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\ConsoleOutputInterface;
 use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\OutputInterface;
--- tests/Command/DaemonCommandTest.php
+++ tests/Command/DaemonCommandTest.php
@@ -2,6 +2,7 @@
 
 namespace Phlib\Console\Tests\Command;
 
+use Symfony\Component\Console\Tester\CommandTester;
 use Phlib\Console\Command\DaemonCommand;
 use phpmock\phpunit\PHPMock;
 
@@ -19,26 +20,45 @@ class DaemonCommandTest extends \PHPUnit_Framework_TestCase
 
     public function testBaseCommandClassIsCalled()
     {
+        $pcntl_fork = $this->getFunctionMock('\Phlib\Console\Command', 'pcntl_fork');
+        $pcntl_fork->expects($this->any())
+            ->willReturn(321);
+        $pcntl_setsid = $this->getFunctionMock('\Phlib\Console\Command', 'pcntl_setsid');
+        $pcntl_setsid->expects($this->any())
+            ->willReturn(0);
+        $file_exists = $this->getFunctionMock('\Phlib\Console\Command', 'file_exists');
+        $file_exists->expects($this->any())
+            ->willReturn(false);
+        $is_writable = $this->getFunctionMock('\Phlib\Console\Command', 'is_writable');
+        $is_writable->expects($this->any())
+            ->willReturn(true);
+
         $name = 'foo:bar';
         $command = $this->getMockBuilder('\Phlib\Console\Command\DaemonCommand')
             ->setConstructorArgs([$name])
-            ->setMethods($this->getUnmockedMethods([]))
             ->getMockForAbstractClass();
         $this->assertEquals($name, $command->getName());
+
+        $tester = new CommandTester($command);
+        $tester->execute([
+            'pid-file' => '/var/run/my.pid',
+            'action' => 'start',
+            '-d' => true
+        ]);
     }
 
-    public function testDefaultArgumentsAreAdded()
-    {
-    }
-
-    public function testDefaultSignalCallbacksAreCreated()
-    {
-    }
-
-    public function getUnmockedMethods(array $mockingMethods)
-    {
-        $availableMethods = get_class_methods('\Phlib\Console\Command\DaemonCommand');
-        var_dump(array_diff($availableMethods, $mockingMethods));
-        return array_diff($availableMethods, $mockingMethods);
-    }
+//    public function testDefaultArgumentsAreAdded()
+//    {
+//    }
+//
+//    public function testDefaultSignalCallbacksAreCreated()
+//    {
+//    }
+//
+//    public function getUnmockedMethods(array $mockingMethods)
+//    {
+//        $availableMethods = get_class_methods('\Phlib\Console\Command\DaemonCommand');
+//        var_dump(array_diff($availableMethods, $mockingMethods));
+//        return array_diff($availableMethods, $mockingMethods);
+//    }
 }
