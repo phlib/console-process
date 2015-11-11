40ede20ed03101b4af640320d2ee34a14936baae
Luke Richards <luke.richards@emailcenteruk.com>
2015-11-11 11:12:24 +0000

    Some initial tests for DaemonCommand
    Added tests for Daemon Command class.

--- tests/Command/DaemonCommandTest.php
+++ tests/Command/DaemonCommandTest.php
@@ -3,62 +3,247 @@
 namespace Phlib\Console\Tests\Command;
 
 use Symfony\Component\Console\Tester\CommandTester;
-use Phlib\Console\Command\DaemonCommand;
+use Symfony\Component\Console\Application;
 use phpmock\phpunit\PHPMock;
 
 class DaemonCommandTest extends \PHPUnit_Framework_TestCase
 {
     use PHPMock;
 
+    /**
+     * @var Application
+     */
+    protected $application;
+
+    /**
+     * @var string
+     */
+    protected $commandName = 'foo:bar';
+
+    /**
+     * @var \TestCommand|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $command;
+
+    /**
+     * @var CommandTester
+     */
+    protected $tester;
+
+    public static function setUpBeforeClass()
+    {
+        require_once __DIR__ . '/files/TestCommand.php';
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->application = new Application();
+        $this->application->add(new \TestCommand($this->commandName));
+
+        $this->command = $this->application->find($this->commandName);
+        $this->tester = new CommandTester($this->command);
+        $this->command->setOutputCallback(function () {
+            return $this->tester->getOutput();
+        });
+    }
+
+    public function tearDown()
+    {
+        $this->tester = null;
+        $this->command = null;
+        $this->application = null;
+        parent::tearDown();
+    }
+
     public function testInstanceOfConsoleCommand()
     {
-        $command = $this->getMockBuilder('\Phlib\Console\Command\DaemonCommand')
-            ->disableOriginalConstructor()
-            ->getMockForAbstractClass();
-        $this->assertInstanceOf('\Symfony\Component\Console\Command\Command', $command);
+        $this->assertInstanceOf('\Symfony\Component\Console\Command\Command', $this->command);
     }
 
     public function testBaseCommandClassIsCalled()
     {
-        $pcntl_fork = $this->getFunctionMock('\Phlib\Console\Command', 'pcntl_fork');
-        $pcntl_fork->expects($this->any())
-            ->willReturn(321);
-        $pcntl_setsid = $this->getFunctionMock('\Phlib\Console\Command', 'pcntl_setsid');
-        $pcntl_setsid->expects($this->any())
-            ->willReturn(0);
-        $file_exists = $this->getFunctionMock('\Phlib\Console\Command', 'file_exists');
-        $file_exists->expects($this->any())
-            ->willReturn(false);
-        $is_writable = $this->getFunctionMock('\Phlib\Console\Command', 'is_writable');
-        $is_writable->expects($this->any())
-            ->willReturn(true);
+        $this->assertEquals($this->commandName, $this->command->getName());
+    }
 
-        $name = 'foo:bar';
-        $command = $this->getMockBuilder('\Phlib\Console\Command\DaemonCommand')
-            ->setConstructorArgs([$name])
-            ->getMockForAbstractClass();
-        $this->assertEquals($name, $command->getName());
+    public function testPidFileArgumentIsAdded()
+    {
+        $this->assertTrue($this->command->getDefinition()->hasArgument('pid-file'));
+    }
 
-        $tester = new CommandTester($command);
-        $tester->execute([
-            'pid-file' => '/var/run/my.pid',
+    public function testActionArgumentIsAdded()
+    {
+        $this->assertTrue($this->command->getDefinition()->hasArgument('action'));
+    }
+
+    public function testDaemonOptionIsAdded()
+    {
+        $this->assertTrue($this->command->getDefinition()->hasOption('daemonize'));
+    }
+
+    public function testDefaultSignalCallbacksAreCreated()
+    {
+        $this->setupStartFunctions(123);
+
+        $pcntl_signal = $this->getFunctionMock('\Phlib\Console\Command', 'pcntl_signal');
+        $pcntl_signal->expects($this->exactly(2))
+            ->withConsecutive($this->onConsecutiveCalls(SIGTERM, SIGINT));
+
+        $this->tester->execute([
+            'pid-file' => '/path/to/my.pid',
             'action' => 'start',
             '-d' => true
         ]);
     }
 
-//    public function testDefaultArgumentsAreAdded()
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testForkFails()
+    {
+        $this->setupStartFunctions(-1);
+        $pcntl_signal = $this->getFunctionMock('\Phlib\Console\Command', 'pcntl_signal');
+        $pcntl_signal->expects($this->any())
+            ->will($this->returnValue(true));
+
+        $this->tester->execute([
+            'pid-file' => '/path/to/my.pid',
+            'action' => 'start',
+            '-d' => true
+        ]);
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testChildFailsToGetSession()
+    {
+        $this->setupStartFunctions(null, -1);
+        $pcntl_signal = $this->getFunctionMock('\Phlib\Console\Command', 'pcntl_signal');
+        $pcntl_signal->expects($this->any())
+            ->will($this->returnValue(true));
+
+        $this->tester->execute([
+            'pid-file' => '/path/to/my.pid',
+            'action' => 'start',
+            '-d' => true
+        ]);
+    }
+
+    public function testChildExecutesSuccessfully()
+    {
+        $expected = 'doExecute called';
+        $this->command->setExecuteOutput($expected);
+        $this->setupStartFunctions(null);
+        $pcntl_signal = $this->getFunctionMock('\Phlib\Console\Command', 'pcntl_signal');
+        $pcntl_signal->expects($this->any())
+            ->will($this->returnValue(true));
+
+        $this->tester->execute([
+            'pid-file' => '/path/to/my.pid',
+            'action' => 'start',
+            '-d' => true
+        ]);
+        $this->assertEquals("$expected\n", $this->tester->getDisplay());
+    }
+
+    public function testChildCallsOnShutdown()
+    {
+        $expected = 'onShutdown called';
+        $this->command->setShutdownOutput($expected);
+        $this->setupStartFunctions(null);
+        $pcntl_signal = $this->getFunctionMock('\Phlib\Console\Command', 'pcntl_signal');
+        $pcntl_signal->expects($this->any())
+            ->will($this->returnValue(true));
+
+        $this->tester->execute([
+            'pid-file' => '/path/to/my.pid',
+            'action' => 'start',
+            '-d' => true
+        ]);
+        $this->assertEquals("$expected\n", $this->tester->getDisplay());
+    }
+
+    public function testStoppingSuccessfully()
+    {
+        $expected = 231;
+        $this->setupStopFunctions($expected);
+
+        $posix_kill = $this->getFunctionMock('\Phlib\Console\Command', 'posix_kill');
+        $posix_kill->expects($this->atLeast(2))->with($this->equalTo($expected))->willReturn(false);
+
+        $this->tester->execute([
+            'pid-file' => '/path/to/my.pid',
+            'action' => 'stop'
+        ]);
+    }
+
+//    /**
+//     * @expectedException \RuntimeException
+//     */
+//    public function testStoppingWhenInvalidPidFile()
 //    {
-//    }
+//        $expected = 231;
+//        $this->setupStopFunctions($expected);
 //
-//    public function testDefaultSignalCallbacksAreCreated()
-//    {
-//    }
+//        $posix_kill = $this->getFunctionMock('\Phlib\Console\Command', 'posix_kill');
+//        $posix_kill->expects($this->atLeast(2))->with($this->equalTo($expected))->willReturn(false);
 //
-//    public function getUnmockedMethods(array $mockingMethods)
-//    {
-//        $availableMethods = get_class_methods('\Phlib\Console\Command\DaemonCommand');
-//        var_dump(array_diff($availableMethods, $mockingMethods));
-//        return array_diff($availableMethods, $mockingMethods);
+//        $this->tester->execute([
+//            'pid-file' => '/path/to/my.pid',
+//            'action' => 'stop'
+//        ]);
 //    }
+
+    protected function setupStartFunctions($fork, $setsid = 0, $fexists = false, $writeable = true, $putContents = true)
+    {
+        $namespace = '\Phlib\Console\Command';
+
+        $pcntl_fork = $this->getFunctionMock($namespace, 'pcntl_fork');
+        $pcntl_fork->expects($this->any())->willReturn($fork);
+
+        $posix_setsid = $this->getFunctionMock($namespace, 'posix_setsid');
+        $posix_setsid->expects($this->any())->willReturn($setsid);
+
+        $file_exists = $this->getFunctionMock($namespace, 'file_exists');
+        $file_exists->expects($this->any())->willReturn($fexists);
+
+        $is_writable = $this->getFunctionMock($namespace, 'is_writable');
+        $is_writable->expects($this->any())->willReturn($writeable);
+
+        $is_writable = $this->getFunctionMock($namespace, 'file_put_contents');
+        $is_writable->expects($this->any())->willReturn($putContents);
+
+        $is_writable = $this->getFunctionMock($namespace, 'unlink');
+        $is_writable->expects($this->any())->willReturn(true);
+
+        $is_writable = $this->getFunctionMock($namespace, 'sleep');
+        $is_writable->expects($this->any())->willReturn(true);
+    }
+
+    protected function setupStopFunctions($pid, $fexists = true, $opened = true, $withPosixKill = false)
+    {
+        $namespace = '\Phlib\Console\Command';
+
+        $file_exists = $this->getFunctionMock($namespace, 'file_exists');
+        $file_exists->expects($this->any())->willReturn($fexists);
+
+        $fopen = $this->getFunctionMock($namespace, 'fopen');
+        $fopen->expects($this->any())->willReturn($opened);
+
+        $fgets = $this->getFunctionMock($namespace, 'fgets');
+        $fgets->expects($this->any())->willReturn($pid);
+
+        $fclose = $this->getFunctionMock($namespace, 'fclose');
+        $fclose->expects($this->any())->willReturn(true);
+
+        if ($withPosixKill) {
+            $posix_kill = $this->getFunctionMock($namespace, 'posix_kill');
+            $posix_kill->expects($this->any())->willReturn(false);
+        }
+
+        $usleep = $this->getFunctionMock($namespace, 'usleep');
+        $usleep->expects($this->any())->willReturn(true);
+    }
 }
---
+++ tests/Command/files/TestCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+use Phlib\Console\Command\DaemonCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TestCommand extends DaemonCommand
+{
+    protected $executeValue = null;
+    protected $shutdownValue = null;
+    protected $outputCallback;
+
+    protected function doExecute(InputInterface $input, OutputInterface $output)
+    {
+        if (!is_null($this->executeValue)) {
+            $output->writeln($this->executeValue);
+        }
+        $this->shutdown();
+    }
+    protected function onShutdown(InputInterface $input, OutputInterface $output)
+    {
+        if (!is_null($this->shutdownValue)) {
+            $output->writeln($this->shutdownValue);
+        }
+    }
+
+    public function setExecuteOutput($value)
+    {
+        $this->executeValue = $value;
+        return $this;
+    }
+
+    public function setShutdownOutput($value)
+    {
+        $this->shutdownValue = $value;
+        return $this;
+    }
+
+    protected function createChildOutput()
+    {
+        return call_user_func($this->outputCallback);
+    }
+
+    public function setOutputCallback(\Closure $callback)
+    {
+        $this->outputCallback = $callback;
+        return $this;
+    }
+}
