0dee4b7d8b96ad09efe296dbef05f2a058e1c3b5
Chris Minett <1084019+chrisminett@users.noreply.github.com>
2024-02-02 16:26:22 +0000

    Upgrade ECS to v12.1
    New config syntax.
    'common' no longer includes 'strict'.

--- ecs.php
+++ ecs.php
@@ -5,24 +5,31 @@ declare(strict_types=1);
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->paths([
-        __DIR__ . '/example',
+return ECSConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
+    ])
 
-    $ecsConfig->sets([
+    ->withSets([
         SetList::COMMON,
         SetList::PSR_12,
-    ]);
+        SetList::STRICT,
+    ])
 
-    $ecsConfig->skip([
+    ->withSkip([
         // Remove sniff, from common/control-structures
         \PhpCsFixer\Fixer\ClassNotation\OrderedClassElementsFixer::class,
 
         // Remove sniff, from common/spaces
         \PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer::class,
         \PhpCsFixer\Fixer\CastNotation\CastSpacesFixer::class,
-    ]);
-};
+    ])
+
+    // PER Coding Style 7.1: "The `fn` keyword MUST NOT be succeeded by a space."
+    ->withConfiguredRule(
+        \PhpCsFixer\Fixer\FunctionNotation\FunctionDeclarationFixer::class,
+        [
+            'closure_fn_spacing' => 'none',
+        ]
+    );
