2f0f637f443af16724d0121728cd4f68293e8a04
Chris Minett <1084019+chrisminett@users.noreply.github.com>
2025-01-27 16:53:31 +0000

    Update ECS

--- ecs.php
+++ ecs.php
@@ -10,6 +10,7 @@ return ECSConfig::configure()
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
+    ->withRootFiles()
 
     ->withSets([
         SetList::COMMON,
@@ -23,13 +24,28 @@ return ECSConfig::configure()
 
         // Remove sniff, from common/spaces
         \PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer::class,
-        \PhpCsFixer\Fixer\CastNotation\CastSpacesFixer::class,
     ])
 
-    // PER Coding Style 7.1: "The `fn` keyword MUST NOT be succeeded by a space."
+    // Rule from common/spaces. No space after cast.
+    ->withConfiguredRule(
+        \PhpCsFixer\Fixer\CastNotation\CastSpacesFixer::class,
+        [
+            'space' => 'none',
+        ],
+    )
+
+    /*
+     * Rule missing from PSR12. PER Coding Style 3:
+     * "... each of the blocks [of import statements] MUST be separated by a single blank line ..."
+     */
+    ->withRules([
+        \PhpCsFixer\Fixer\Whitespace\BlankLineBetweenImportGroupsFixer::class,
+    ])
+
+    // Rule from PSR12. PER Coding Style 7.1: "The `fn` keyword MUST NOT be succeeded by a space."
     ->withConfiguredRule(
         \PhpCsFixer\Fixer\FunctionNotation\FunctionDeclarationFixer::class,
         [
             'closure_fn_spacing' => 'none',
-        ]
+        ],
     );
